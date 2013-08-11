64103b20b371602ff98bf5cd60139e5a48833859
OopsMonk <oopsmonk@gmail.com>
2013-08-11 22:50:00 +0800

    add function list.

--- pyWebMOC.py
+++ pyWebMOC.py
@@ -5,6 +5,7 @@
 from bottle import route, static_file, debug, run, get, view, redirect
 from bottle import post, request, response
 import os, inspect, json
+import moc
 
 #enable bottle debug
 debug(True)
@@ -29,6 +30,73 @@ def html_file(filename):
 @get(routePath + '/assets/<filepath:path>')
 def assets_file(filepath):
     return static_file(filepath, root=rootPath+'/assets')
+
+"""
+Function list
+* Player control GET
+    * return moc server state (play/stop/pause/not running)
+
+* Player control POST
+    * Previous
+    * Play / Resume
+    * Next
+    * Pause
+    * Stop
+    * volume
+
+* Player info
+    * Song name
+    * current time
+    * duration
+    * 
+"""
+
+#control GET API return 
+@get(routePath + '/ctl')
+def MOCControlGET():
+    """
+    -1: moc server not running 
+    0: Stopping
+    1: Pause
+    2: Playing
+    """
+    st = moc.get_state()
+    print "Get moc server status : %d" % st
+    return json.dumps({'Status':st})
+
+#handle control post action
+def CtlHandle(data):
+    """
+    request:
+    {'do':action}
+    action:
+    0: Prev 
+    1: Play/Pause
+    2: Next
+    3: Stop
+    4: Volume
+    5: Add playlist
+    6: append playlist
+    7: clear playlist
+    8:
+    response:
+    {'ack': error}
+    error:
+    0: Success
+    not zero: Failed.
+    """
+
+    return json.dumps({'ack':0})
+
+#control POST API
+@post(routePath + '/ctl')
+def MOCControlPOST():
+    data = request.json
+    if data == None:
+        return json.dumps({'ack': -1})
+
+    print "Get cmd : %s" % data 
+    return CtlHandle(data)
 
 #http://gotofritz.net/blog/weekly-challenge/restful-python-api-bottle/
 """
